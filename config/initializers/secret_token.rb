f0b3e60a005167897f5713d9b76b3542fa14ee1b
Derek Hammer <derek.r.hammer@gmail.com>
2014-07-28 14:34:40 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TodoBackendRails::Application.config.secret_key_base = 'c1ab0a89ab91659c92808287f5d6056ff50c9e87b9a074e5dce08d56ea4fedf2e497ce09355155542ed66555eccf891629f0764ccfbeefc641919e5aadf3f28c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TodoBackendRails::Application.config.session_store :cookie_store, key: '_todo-backend-rails_session'
