257800bf571347e1046043858267a3f767290c27
Derek Hammer <derek.r.hammer@gmail.com>
2014-08-09 18:26:26 -0700

    Controller test

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe HomeController, :type => :controller do
-
+  context 'GET index' do
+    it 'should respond successfully' do
+      get :index
+      expect(response).to be_successful
+    end
+  end
 end
