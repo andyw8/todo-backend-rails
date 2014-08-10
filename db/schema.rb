c0fcdfc2a17b9281190b4ee9e56368a27c8ea635
Derek Hammer <derek.r.hammer@gmail.com>
2014-08-09 19:09:26 -0700

    Add completed (default false) to todo

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140810015504) do
+ActiveRecord::Schema.define(version: 20140810020752) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20140810015504) do
     t.string   "title"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "completed",  default: false
   end
 
 end
