5c7116b3d90dab8e777f0c3aea10640fb8a06bc0
Derek Hammer <derek.r.hammer@gmail.com>
2014-08-09 19:22:06 -0700

    Include URL

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,9 @@
 class Todo < ActiveRecord::Base
+  def url
+    "/#{id}"
+  end
+
+  def serializable_hash config
+    super(config).merge(url: url)
+  end
 end
---
+++ spec/models/todo_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Todo, :type => :model do
+  subject { Todo.new(title: 'test todo', id: 2) }
+  it 'should have url' do
+    expect(subject.url).to eq '/2'
+  end
+  it 'should include url in json output' do
+    expect(subject.to_json).to include 'url'
+  end
+end
